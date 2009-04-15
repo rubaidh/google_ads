78980a1ec6a49f1592b7b708c15c234bee7eae8f
Mark Connell <mark@rubaidh.com>
2009-04-15 12:47:07 +0100

    Update plugin to use google's simplified javascript since you can do all the ad changes on googles end now

--- lib/rubaidh/google_ads.rb
+++ lib/rubaidh/google_ads.rb
@@ -2,17 +2,10 @@ module Rubaidh
   module GoogleAds
     mattr_accessor :defaults
     @@defaults = {
-      :client         => 'pub-invalid-client',
-      :width          => 200,
-      :height         => 200,
-      :type           => :text_image,
-      :channel        => nil,
-      :border_colour  => 'FFFFFF',
-      :bg_colour      => 'FFFFFF',
-      :link_colour    => '0000FF',
-      :text_colour    => '000000',
-      :url_colour     => '00FF00',
-      :ui_features    => 'rc:0'
+      :client   => 'pub-invalid-client',
+      :width    => 200,
+      :height   => 200,
+      :ad_slot  => 'invalid_ad_slot'
     }
     
     mattr_accessor :ad_formats
--- lib/rubaidh/google_ads/tag_helper.rb
+++ lib/rubaidh/google_ads/tag_helper.rb
@@ -9,22 +9,15 @@ module Rubaidh
         # Spit out a google ad with the appropriate settings pulled from
         # our configuration
         def google_ad(format = :default)
+
           return unless GoogleAds.enabled?
-          
+
           options = Rubaidh::GoogleAds.defaults.merge(Rubaidh::GoogleAds.ad_formats[format])
           js = javascript_tag <<-JS
             google_ad_client = "#{options[:client]}";
             google_ad_width = #{options[:width]};
             google_ad_height = #{options[:height]};
-            google_ad_format = "#{options[:width]}x#{options[:height]}_as";
-            google_ad_type = "#{options[:type]}";
-            google_ad_channel = "#{options[:channel]}";
-            google_color_border = "#{options[:border_colour]}";
-            google_color_bg = "#{options[:bg_colour]}";
-            google_color_link = "#{options[:link_colour]}";
-            google_color_text = "#{options[:text_colour]}";
-            google_color_url = "#{options[:url_colour]}";
-            google_ui_features = "#{options[:ui_features]}";
+            google_ad_slot = "#{options[:ad_slot]}";
           JS
           js << "\n"
           js << javascript_include_tag("http://pagead2.googlesyndication.com/pagead/show_ads.js")
